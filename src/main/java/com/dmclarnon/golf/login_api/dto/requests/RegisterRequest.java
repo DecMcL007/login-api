dd0c7fd8cef5b3c7419b6495f866ba5d5903d965
D McLarnon <dmclarnon@Ds-Mac-mini.local>
2025-09-15 13:24:26 +0100

    add PasswordsMatch validation annotation

--- src/main/java/com/dmclarnon/golf/login_api/dto/requests/RegisterRequest.java
+++ src/main/java/com/dmclarnon/golf/login_api/dto/requests/RegisterRequest.java
@@ -1,11 +1,14 @@
 package com.dmclarnon.golf.login_api.dto.requests;
 
+import com.dmclarnon.golf.login_api.validation.PasswordsMatch;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@PasswordsMatch
 public record RegisterRequest(
-    @NotBlank String username,
-    @Email @NotBlank String email,
-    @Size(min = 8) String password
+        @NotBlank String username,
+        @Email @NotBlank String email,
+        @Size(min = 8) String password1,
+        @Size(min = 8) String password2
 ) {}
