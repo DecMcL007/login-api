f63dc4adc9e57756797a60231462a3bce5590ff4
D McLarnon <dmclarnon@Ds-Mac-mini.local>
2025-10-30 21:36:49 +0000

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+      backend "s3" {
+        bucket         = "golf-terraform-state"
+        key            = "login-api/terraform.tfstate"
+        region         = "eu-west-1"
+        dynamodb_table = "terraform-locks"
+      }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
